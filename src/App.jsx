ea798090e93a6e2138ccb90e9b56b74517531365
Javalopes <flopestv@gmail.com>
2026-02-22 22:09:19 +0000

    vercel

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className='bg-slate-950 h-screen flex items-center justify-center text-cyan-400 font-mono'>
+      <h1>HASH EMPIRE V2.0 - SISTEMA INICIALIZADO</h1>
+    </div>
+  );
+}
