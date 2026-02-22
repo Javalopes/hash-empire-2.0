8eb336488f2a36778d2094121caa849827d3cdbd
Javalopes <flopestv@gmail.com>
2026-02-22 21:58:52 +0000

    vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cyan: '#06b6d4',
+        'neon-pink': '#f472b6',
+      },
+    },
   },
   plugins: [],
 };
