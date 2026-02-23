db90234d9731a561187fcd82b33e51c637f19cbb
Javalopes <flopestv@gmail.com>
2026-02-23 02:58:08 +0000

    fix?

--- src/components/game/core/GameStage.jsx
+++ src/components/game/core/GameStage.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
-import { useAuth } from '../../../lib/AuthContext.jsx';
 import { Stage, Container } from '@pixi/react';
-import { useAuth } from '../../../lib/AuthContext.jsx';
 import useGameSync from '../../../hooks/useGameSync.js';
 import BackgroundLayer from '../layers/BackgroundLayer.jsx';
 import MultiplayerLayer from '../layers/MultiplayerLayer.jsx';
